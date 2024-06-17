c9cae405f4d2cf81803de28718cb8f5745b5815e
KT <chan.KT@inventec.com>
2024-06-17 15:32:06 +0800

    1. 自訂模型資料驗證標籤: 在 Dto 內驗證 寫法

--- Dtos/PostReqDto.cs
+++ Dtos/PostReqDto.cs
@@ -1,12 +1,11 @@
-﻿using Post.ValidationAttributes;
+﻿using Post.Models;
+using Post.ValidationAttributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace Post.Dtos
 {
-    [PassValueAttribute(PassValue = "123")]
-    [StartTimeEndTimeAttribute]
-    public class PostReqDto
+    public class PostReqDto : IValidatableObject
     {
-        [PostTitleAttribute] // 也可寫成 [PostTitle]
         public string Title { get; set; }
         public string Description { get; set; }
         public string ImagePath { get; set; }
@@ -14,5 +13,37 @@ namespace Post.Dtos
         public DateTime StartTime { get; set; }
 
         public DateTime EndTime { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            // 取得 service
+            PostContext _postContext = (PostContext)validationContext.GetService(typeof(PostContext));
+
+            var findTitle = from a in _postContext.PostLists
+                            where a.Title == Title
+                            select a;
+
+            // 確認傳入的 Dto 是否是 PutReqDto
+            var dto = validationContext.ObjectInstance;
+
+            if (dto.GetType() == typeof(PutReqDto))
+            {
+                var dtoUpate = (PutReqDto)dto;
+                findTitle = findTitle.Where(a => a.Id != dtoUpate.Id);
+            }
+
+            if (findTitle.FirstOrDefault() != null)
+            {
+                yield return new ValidationResult("已存在相同的代辦事項", ["title"]);
+            }
+
+
+            if (StartTime >=EndTime)
+            {
+                yield return new ValidationResult("開始時間不可以大於結束時間", ["time"]);
+            }
+
+            yield return ValidationResult.Success;
+        }
     }
 }
